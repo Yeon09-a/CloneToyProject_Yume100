0dc10f69571bff85b6be9f3b87e8ec0a083ed168
Yeon09-a <kimyeona0926@gmail.com>
2022-10-19 11:39:51 +0900

    2022.10.18 메인 UI 제작
    2022.10.18
    메인 UI 제작

--- CloneYume100/Assets/02.Scripts/StartScene/StartManager.cs
+++ CloneYume100/Assets/02.Scripts/StartScene/StartManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
@@ -13,6 +15,14 @@ public class StartManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount >= 1 && EventSystem.current.IsPointerOverGameObject() == false) // 화면 터치 시 player 이동
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                SceneManager.LoadScene("MainUI");
+            }
+        }
     }
 }
